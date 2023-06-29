631d643025d29c567c4da513ab405808c89d6dd2
Neethu Sasidharan Nair <86210328+nees101@users.noreply.github.com>
2023-06-29 18:10:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-$(".email").on("input",function(){
-  if($(".email").val()==""){
-    $(".error").css("display","none");
-    $(".email").css("border","1px solid black");
-    $(".email").css("color","black").val();
+$("#email").on("input",function(){
+  if($("#email").val()==""){
+    $("#error").css("display","none");
+    $("#email").css("border","1px solid black");
+    $("#email").css("color","black").val();
   }
 });
 
@@ -14,10 +14,10 @@ $(".subscribe").on("click",function(e){
         var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
         return expr.test(email);
     };
-    if (!ValidateEmail($(".email").val())) {
+    if (!ValidateEmail($("#email").val())) {
             $(".error").css("display","block");
-            $(".email").css("border","2px solid red");
-            $(".email").css("color","red").val();
+            $("#email").css("border","2px solid red");
+            $("#email").css("color","red").val();
         }
         else {
             $(".sign-up-form").css("display","none");
@@ -27,6 +27,6 @@ $(".subscribe").on("click",function(e){
 
   $(".dismiss-btn").on("click",function(){
     $(".sign-up-form").css("display","block");
-        $(".email").val("");
+        $("#email").val("");
     $(".success-message").css("display","none");
   });
